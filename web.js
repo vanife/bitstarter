e5ed13dc3a5dff2601f713f2bb49cc3b3f1ecea3
Ivan Melnychuk <contact.vano+github@gmail.com>
2013-08-16 13:22:29 +0000

    reading from the file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var cont = fs.readFileSync('index.html', encoding='ascii');
+    console.log(cont);
+    //var cont = 'Hello World 2!';
+
+    response.send(cont);
 });
 
 var port = process.env.PORT || 5000;
